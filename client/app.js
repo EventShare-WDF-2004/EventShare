dc5a2af6dad975bbafe887af5f24c50810bebbca
serge aristide <nsergearistide@gmail.com>
2020-07-15 19:56:28 -0400

    added: footer

--- client/app.js
+++ client/app.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
 import {Navbar} from './components'
+import {Footer} from './components'
+
 import Routes from './routes'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Footer />
       <Routes />
     </div>
   )
